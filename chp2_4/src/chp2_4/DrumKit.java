519d76bffebc4747ca3622625b9f819f2bff3389
Freshield <zxdsw199182@gmail.com>
2016-01-21 22:12:46 -0500

    assignment 1

---
+++ chp2_4/src/chp2_4/DrumKit.java
@@ -0,0 +1,23 @@
+package chp2_4;
+
+public class DrumKit {
+	
+	boolean topHat = true;
+	boolean sanre = true;
+	
+	
+	
+	void playSnare()
+	{
+		System.out.println("bang bang ba-bang");
+		
+		
+	}
+	
+	void playTopHat()
+	{
+		System.out.println("ding ding da-ding");
+		
+	}
+
+}
---
+++ chp2_4/src/chp2_4/DrumKitTestDrive.java
@@ -0,0 +1,22 @@
+package chp2_4;
+
+public class DrumKitTestDrive {
+
+	public static void main(String[] args) {
+		
+		DrumKit d = new DrumKit();
+		d.playSnare();
+		d.sanre = false;
+		if(d.sanre == true)
+		{
+			d.playSnare();
+		}
+		d.playTopHat();
+		
+
+		
+		
+
+	}
+
+}
