e14e89228e8e7db2b7ecb733e11f3a0cadd50728
Freshield <zxdsw199182@gmail.com>
2016-01-27 14:57:25 -0500

    chp4 2

---
+++ chp4_2/src/chp4_2/Dog.java
@@ -0,0 +1,26 @@
+package chp4_2;
+
+public class Dog {
+	int size;
+	String name;
+	
+	void bark()
+	{
+		if(size > 60)
+		{
+			System.out.println("Woof!");
+		}
+		else if(size > 14)
+		{
+			System.out.println("Ruff!");
+		}
+		else
+		{
+			System.out.println("Yip!");
+		}
+		
+		
+	}
+	
+
+}
