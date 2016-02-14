5ff89e0179e9aa724983570f1e5007e4ffed0bf8
Freshield <zxdsw199182@gmail.com>
2016-02-14 14:06:24 -0500

    chp9 1

---
+++ chp9_1/src/chp9_1/Duck.java
@@ -0,0 +1,17 @@
+package chp9_1;
+
+public class Duck {
+	int size;
+	
+	public Duck()
+	{
+		size = 34;
+		System.out.println("Quack");
+	}
+	
+	public void setSize(int newSize)
+	{
+		size = newSize;
+	}
+
+}
