113b7ec096a5fa83b50b7d667ed6ef2c3c96751d
Freshield <zxdsw199182@gmail.com>
2016-02-15 11:57:31 -0500

    chp10 1

---
+++ chp10_1/src/chp10_1/TestMath.java
@@ -0,0 +1,15 @@
+package chp10_1;
+
+public class TestMath {
+
+	public static void main(String[] args) {
+		
+		long x = Math.round(42.2);
+		int y = Math.min(56, 12);
+		int z = Math.abs(-343);
+		
+		System.out.println(x+" "+y+" "+z);
+
+	}
+
+}
