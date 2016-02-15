09ef622912e1206fa01fb4e98ccfdbbb85c9bd84
Freshield <zxdsw199182@gmail.com>
2016-02-15 18:23:59 -0500

    chp10 0

---
+++ chp10_0/src/chp10_0/StaticSuper.java
@@ -0,0 +1,15 @@
+package chp10_0;
+
+public class StaticSuper {
+	
+	static
+	{
+		System.out.println("super static block");
+	}
+	
+	StaticSuper()
+	{
+		System.out.println("super constructor");
+	}
+
+}
