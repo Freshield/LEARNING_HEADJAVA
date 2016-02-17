326e6360a79ab09106e0abbbf9f8b46660872a9e
Freshield <zxdsw199182@gmail.com>
2016-02-16 19:32:41 -0500

    chp12 5

---
+++ chp12_5/src/chp12_5/MyDrawPanel.java
@@ -0,0 +1,41 @@
+package chp12_5;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MyDrawPanel extends JPanel{
+	
+	
+	public void paintComponent(Graphics g){
+		
+		//g.setColor(Color.orange);
+		
+		//g.fillRect(20, 50, 100, 100);
+		
+		int red = getRandom();
+		int green = getRandom();
+		int blue = getRandom();
+		Color startColor = new Color(red,green,blue);
+		
+		red = getRandom();
+		green = getRandom();
+		blue = getRandom();
+		Color endColor = new Color(red,green,blue);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		GradientPaint gradient = new GradientPaint(70,70,startColor,150,150,endColor);
+		
+		g2d.setPaint(gradient);
+		
+		g2d.fillOval(70, 70, 100, 100);
+		
+		
+	}
+	
+	public int getRandom(){
+		
+		return (int) (Math.random()*255);
+	}
+
+}
